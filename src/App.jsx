b236bf1068171012f697ac5f87619369d0aba7c6
SathiraPerera <18032945-SathiraPerera@users.noreply.replit.com>
2023-05-05 06:05:33 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+import Favorites from './Components/Favorites'
+import Meals from './Components/Meals'
+import Modal from './Components/Modal'
+import Search from './Components/Search'
+
+
+export default function App() {
+  return (
+    <main>
+      {/* <Search/> */}
+      {/* <Favorites/> */}
+      <Meals/>
+      {/* <Modal/> */}
+
+    </main>
+  )
+}
