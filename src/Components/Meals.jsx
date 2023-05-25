58e608499aa239bf2c592bb7a183447b84f94b79
sathirap-cv <118736300+sathirap-cv@users.noreply.github.com>
2023-05-25 10:24:45 +0530

    Commit two

--- src/Components/Meals.jsx
+++ src/Components/Meals.jsx
@@ -1,5 +1,19 @@
+import { useGlobalContext } from '../context'
+
 const Meals = () => {
-  return <h1>Meals Component</h1>
+  const { meals } = useGlobalContext()
+
+  return <section className="section-center">
+    {meals.map((singleMeal) => {
+      const { idMeal, strMeal: title, strMealThumb: image } = singleMeal
+      return <article  key={idMeal} className="single-meal">
+      <img src={image} className="img"/>
+        <footer>
+        <h5>{title}</h5>
+        </footer>
+        </article>
+    })}
+  </section>
 }
 
 export default Meals
